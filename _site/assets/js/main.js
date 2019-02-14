83af18c198cb79aa4dc6872aedec48ca042ab91b
emCog <robin.leonard@gmail.com>
2019-02-14 22:02:47 +0000

    updates to desktop ui

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,13 +1,13 @@
-// play pause
-const playPauseBtn = document.getElementbyId('amplitude-play-pause');
+// // play pause
+// const playPauseBtn = document.getElementbyId('amplitude-play-pause');
 
-playPauseBtn.addEventListener('mousedown', playPause);
-playPauseBtn.addEventListener('touchstart', playPause);
+// playPauseBtn.addEventListener('mousedown', playPause);
+// playPauseBtn.addEventListener('touchstart', playPause);
 
-function playPause() {
-	if(this.classList.contains('player_is-playing') {
-		this.classList.remove('player_is-playing');
-	} else {
-		this.classList.add('player_is-playing');
-	}
-}
+// function playPause() {
+// 	if(this.classList.contains('player_is-playing')) {
+// 		this.classList.remove('player_is-playing');
+// 	} else {
+// 		this.classList.add('player_is-playing');
+// 	}
+// }
